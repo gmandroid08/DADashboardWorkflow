bfba0ea438ba515b09b194da3f629b396d399f86
gmandroid08 <gugandroid08@gmail.com>
2025-04-29 16:32:05 -0400

    da_workflow_tracker_live.py

--- da_workflow_tracker_live.py
+++ da_workflow_tracker_live.py
@@ -1,21 +1,23 @@
-# Install needed packages
-# pip install streamlit pandas gspread gspread_dataframe streamlit-aggrid oauth2client
+# Install needed packages:
+# pip install streamlit pandas gspread gspread_dataframe google-auth streamlit-aggrid
 
 import pandas as pd
 import streamlit as st
 import gspread
+from google.oauth2 import service_account
 from gspread_dataframe import get_as_dataframe
-from oauth2client.service_account import ServiceAccountCredentials
 from st_aggrid import AgGrid, GridOptionsBuilder
 
-# Set up credentials and access Google Sheet
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name('dalivedashboard-552a127af3fd.json', scope)
+# Setup credentials from Streamlit Secrets
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["dalivedashboard-552a127af3fd"]
+)
+
 gc = gspread.authorize(credentials)
 
-# Open Google Sheet by URL
+# Connect to Google Sheet
 spreadsheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1T6HbMBJRHgsZFWJiVqwJeEHUE-oVKQ0BvZTRyfPN964/edit?usp=sharing')
-worksheet = spreadsheet.sheet1  # Assuming data is on the first sheet
+worksheet = spreadsheet.sheet1
 df = get_as_dataframe(worksheet).dropna()
 
 # Build Streamlit app
@@ -24,11 +26,9 @@ st.title('Data Associate Live Workflow Tracker')
 
 st.markdown('**Hover or click on DA names to view their current workflow assignment.**')
 
-# Build interactive table
+# Create Interactive Table
 gb = GridOptionsBuilder.from_dataframe(df)
 gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, editable=False)
 gridOptions = gb.build()
 
 AgGrid(df, gridOptions=gridOptions, enable_enterprise_modules=False, theme='streamlit')
-
-st.caption('Built by Gugan L. | GitHub: https://github.com/gmandroid08/DADashboardWorkflow')
