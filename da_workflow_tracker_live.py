f9da9e8a60c8eaaec6241448939456075e99a2fc
gmandroid08 <gugandroid08@gmail.com>
2025-04-29 14:24:09 -0400

    da_workflow_tracker_live.py

--- da_workflow_tracker_live.py
+++ da_workflow_tracker_live.py
@@ -1,20 +1,32 @@
+# Install needed packages:
+# pip install streamlit pandas gspread gspread_dataframe google-auth streamlit-aggrid
+
 import pandas as pd
 import streamlit as st
 import gspread
+from google.oauth2 import service_account
 from gspread_dataframe import get_as_dataframe
-from oauth2client.service_account import ServiceAccountCredentials
 from st_aggrid import AgGrid, GridOptionsBuilder
 
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('552a127af3fd6b0745a86084f1d51bd4ad73e79f', scope)
+# Setup credentials from Streamlit Secrets
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["dalivedashboard-552a127af3fd"]
+)
+
 gc = gspread.authorize(credentials)
 
-sheet = gc.open('Sample_DA_WorkflowDashboard').sheet1
-df = get_as_dataframe(sheet).dropna()
+# Connect to Google Sheet
+spreadsheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1T6HbMBJRHgsZFWJiVqwJeEHUE-oVKQ0BvZTRyfPN964/edit?usp=sharing')
+worksheet = spreadsheet.sheet1
+df = get_as_dataframe(worksheet).dropna()
 
-st.title('DA Live Workflow Tracker')
-st.markdown('**Hover or click on names to see current workflow**')
+# Build Streamlit app
+st.set_page_config(page_title="DA Workflow Tracker", page_icon=":clipboard:", layout="wide")
+st.title('Data Associate Live Workflow Tracker')
 
+st.markdown('**Hover or click on DA names to view their current workflow assignment.**')
+
+# Create Interactive Table
 gb = GridOptionsBuilder.from_dataframe(df)
 gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, editable=False)
 gridOptions = gb.build()
